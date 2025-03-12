0b6a1da9f0115340230d62feafa58422d538d45c
ChillyToaster03 <160430384+ChillyToaster03@users.noreply.github.com>
2025-03-12 23:33:03 +0200

    Create phonebook.c

---
+++ phonebook.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    FILE *file = fopen("phonebook.csv", "a");
+    if (file == NULL)
+    {
+        perror("Error Opennin File");
+        return 1;
+    }
+
+    char *name= malloc(100 * sizeof(char));
+    char *num= malloc(100 * sizeof(char));
+    if (name == NULL || num == NULL)
+    {
+        perror("Memor Allocation Failed");
+        return 1;
+    }
+
+    printf("Enter The Name: ");
+    scanf("%99s", name);
+    printf("Enter The Number: ");
+    scanf("%99s", num);
+
+    fprintf(file, "%s, %s\n", name, num);
+    fclose(file);
+    free(name);
+    free(num);
+return 0;
+}
